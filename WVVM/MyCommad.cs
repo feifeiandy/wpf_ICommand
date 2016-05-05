039f46ed650c00f2a60db1da822183e46f96f0dd
feifeiandy <feifeiandy@outlook.com>
2016-05-05 15:24:16 +0800

    c

---
+++ WVVM/MyCommad.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WVVM
+{
+    public class MyCommad : ICommand
+    {
+        private Action<Object> _execute;
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            this._execute(parameter);
+        }
+
+        public MyCommad(Action<object> execute)
+        {
+            this._execute = execute;
+        }
+    }
+}
---
+++ WVVM/ViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WVVM
+{
+    public class ViewModel
+    {
+        public Student Stu { get; set; }
+        public MyCommad Mcomd { get; set; }
+
+        public ViewModel()
+        {
+            Stu = new Student() { Pid = 1, Pname = "张三" };
+            Mcomd = new MyCommad(SetText);
+        }
+
+        private void SetText(object str)
+        {
+            Stu.Pname = "李四";
+        }
+    }
+
+    public class Student : INotifyPropertyChanged
+    {
+        private int pid;
+
+        public int Pid
+        {
+            get { return pid; }
+            set 
+            {
+                pid = value;
+                
+                //此处要判断是否订阅事件
+                if (this.PropertyChanged != null)
+                {
+                    OnPropertyChanged("Pid");
+                }
+            }
+        }
+
+        private string pname;
+
+        public string Pname
+        {
+            get { return pname; }
+            set
+            {
+                pname = value;
+                if (this.PropertyChanged != null)
+                {
+                    OnPropertyChanged("Pname");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
